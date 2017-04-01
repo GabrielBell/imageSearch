194f4c77190a9c70d4d9b3fdbb6ff556b815c0ae
Gabriel Bell <gmbell23@gmail.com>
2017-03-31 23:56:51 -0400

    added google cse

--- routes/imageSearch.js
+++ routes/imageSearch.js
@@ -1,6 +1,9 @@
 var express= require('express');
 var router= express.Router();
 var Search= require('../models/search');
+var request= require('request');
+var api_key="key="+process.env.GOOGLE_API+"&";
+var cse_ID="cx="+process.env.GOOGLE_CSE_ID+"&"
 
 router.use(function timeLog(req,res,next){
 	console.log('Time', Date.now());
@@ -9,19 +12,31 @@ router.use(function timeLog(req,res,next){
 
 
 router.get('/*', function(req,res){
+	//make sure query is a rational number
 	var query= req.query.offset;
-	
+	if(!/^\d{1,2}$/.test(query)){ query="10"}
 	//verify/sanitize input of req.url
 	var terms= /\/([\w]+[\s\w]*)\??/g.exec(decodeURIComponent(req.url))[1].split(' ');
-	console.log(terms)
+	
 	//perform check before db update
 	var s = new Search({'when': Date.now(), 'terms':terms});
 	s.save(function(err,doc,num){
 		if(err) throw err;
-		if(num) console.log('added ', doc, 'to db')
+		if(num) console.log('added doc to db')
 	})
 	//call API with  [terms,offset]
-	//https://github.com/vadimdemedes/google-images
+	
+	var searchStr= "q="+terms.join('+')+"&";
+	var numImages="num="+query+"&";
+	var googleURI= "https://www.googleapis.com/customsearch/v1?"+api_key+cse_ID+searchStr+numImages+"fields=items";
+	//console.log(googleURI)
+	request(googleURI, function(err,response, body){
+		if(err) console.log('ERROR: ',err);
+		if(response.statusCode === 200){
+			console.log('hurray');
+			console.log(body)
+		}
+	})
 	res.send('imagesearch get endpoint');
 });
 
--- server.js
+++ server.js
@@ -6,19 +6,27 @@ var uri= 'mongodb://localhost:27017/image_db';
 var port = process.env.PORT || 8080;
 
 
-mongoose.connect(uri);
+
+//mongoose.connect(uri); use for working locally
+mongoose.connect(process.env.MONGOLAB_URI); //use for deployment
 var db= mongoose.connection;
 db.on('open', function(){ console.log('connected to mongo')})
-db.on('error', function(){ console.log('unable to connect to mongo')})
+db.on('error', function(err){ console.log("unable to connect"); })
 
 var app= express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.use('/api/imagesearch', imageSearch);
 app.use('/api/latest', latest);
 
-
+app.get('/', function(req,res){
+	res.render('index', {title: 'Image Search Microservice', message:'Welcome to Image Search Microservice'})
+})
 
 
 app.listen(port, function(){
 	console.log('listening on port'+port);
+	
 })
